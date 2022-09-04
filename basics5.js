c760645b0fc9dc8aceb77a0eb3add0872b91f8a9
Riya Tiwari <riyatiwari727@gmail.com>
2022-09-05 02:10:33 +0530

    2

---
+++ basics5.js
@@ -0,0 +1,22 @@
+var passScore = 20
+var score = 18
+var myScore = 19
+if (myScore > score){
+    var joy = "Yayyy!"
+    console.log(joy)
+}
+console.log(passScore)
+console.log(score)
+console.log(myScore)
+
+var passScore = 20
+let myScore = 19
+let joy = "Woah!";
+if (myScore > 18){
+    let joy;
+    joy = "Yayyy!"
+    console.log(joy)
+}
+console.log(passScore)
+console.log(myScore)
+console.log(joy)
